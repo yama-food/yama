860b0ef83a5720a8548cd14a97fd4cd1cf949677
valentinerutto <rutto28@gmail.com>
2015-09-04 10:51:13 +0300

    toast on signup

--- app/src/main/java/com/valentine/yama/MainActivity.java
+++ app/src/main/java/com/valentine/yama/MainActivity.java
@@ -1,25 +1,35 @@
 package com.valentine.yama;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-
-import com.parse.Parse;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends ActionBarActivity {
-
+ Button mbtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Parse.enableLocalDatastore(this);
-        Parse.initialize(this, "eX4tyHBF7yjbN5tPqmbugnu6l9nonJIKLvvMXeI6", "hhfIkPjG0XjiqSktL3WskGnASiPRsxFnuZqWM8Tx");
+    }
+    protected void  OnClickButtonListener(){
+        mbtn=(Button)findViewById(R.id.btn);
+        mbtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("Register");
+                        startActivity(intent);
+                    }
+                }
+        );
 
     }
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
---
+++ app/src/main/java/com/valentine/yama/MyApp.java
@@ -0,0 +1,26 @@
+package com.valentine.yama;
+
+
+import com.parse.Parse;
+import com.parse.ParseACL;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
+
+/**
+ * Created by valentine on 9/3/15.
+ */
+public class MyApp extends android.app.Application{
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Add your initialization code here
+        Parse.initialize(this, "eX4tyHBF7yjbN5tPqmbugnu6l9nonJIKLvvMXeI6", "hhfIkPjG0XjiqSktL3WskGnASiPRsxFnuZqWM8Tx");
+        ParseUser.enableAutomaticUser();
+        ParseACL defaultACL = new ParseACL();
+
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
+}
+}
