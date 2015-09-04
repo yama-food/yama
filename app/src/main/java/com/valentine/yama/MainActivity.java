6f875698aa8c2a02d98b000ee5c2e42572ea31fa
valentinerutto <rutto28@gmail.com>
2015-09-04 13:07:27 +0300

    send user to login activity after selecting the logout on settings

--- app/src/main/java/com/valentine/yama/MainActivity.java
+++ app/src/main/java/com/valentine/yama/MainActivity.java
@@ -57,13 +57,15 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_logout:
+                ParseUser.logOut();
+                Intent intent = new Intent(this, Login.class);
+                startActivity(intent);
+                finish();
+                return true;
+    }
+    return false;
     }
 }
