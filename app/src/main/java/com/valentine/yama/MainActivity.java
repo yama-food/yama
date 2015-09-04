3b12bdc3947662ff1bf49a2fface8e43cffde808
valentinerutto <rutto28@gmail.com>
2015-09-04 12:57:03 +0300

    added logoutaction on menu item

--- app/src/main/java/com/valentine/yama/MainActivity.java
+++ app/src/main/java/com/valentine/yama/MainActivity.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 
 import com.parse.ParseAnonymousUtils;
@@ -44,20 +43,6 @@ public class MainActivity extends ActionBarActivity {
             }
         }
 
-
-    }
-    protected void  OnClickButtonListener(){
-        mbtn=(Button)findViewById(R.id.btn);
-        mbtn.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(MainActivity.this,Register.class);
-
-                        startActivity(intent);
-                    }
-                }
-        );
 
     }
     @Override
