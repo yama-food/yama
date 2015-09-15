8fb360a539e1bd622c9c1b300218eff0a1d09eea
wathika <vcwathika@gmail.com>
2015-09-15 11:48:42 +0100

    added facebook login activity

---
+++ app/src/main/java/com/valentine/yama/FbLoginFragment.java
@@ -0,0 +1,39 @@
+package com.valentine.yama;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.facebook.FacebookSdk;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FbLoginFragment extends Fragment {
+
+
+    public FbLoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getActivity().getApplicationContext());
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        TextView textView = new TextView(getActivity());
+        textView.setText(R.string.hello_blank_fragment);
+        return textView;
+    }
+
+
+}
