0f83a16ae700af77f0ce082b1f75f068de348943
valentinerutto <rutto28@gmail.com>
2015-09-16 21:01:39 +0300

    added welcome layout with logout and continue to app button

--- app/src/main/java/com/valentine/yama/Welcome.java
+++ app/src/main/java/com/valentine/yama/Welcome.java
@@ -1,17 +1,48 @@
 package com.valentine.yama;
 
+import android.app.Activity;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.parse.ParseUser;
 
 
-public class Welcome extends ActionBarActivity {
-
+public class Welcome extends Activity {
+Button logout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        // Retrieve current user from Parse.com
+        ParseUser currentUser = ParseUser.getCurrentUser();
+
+        // Convert currentUser into String
+        String struser = currentUser.getUsername().toString();
+
+        // Locate TextView in welcome.xml
+        TextView txtuser = (TextView) findViewById(R.id.txtuser);
+
+        // Set the currentUser String into TextView
+        txtuser.setText("You are logged in as " + struser);
+
+        // Locate Button in welcome.xml
+        logout = (Button) findViewById(R.id.logout);
+
+        // Logout Button Click Listener
+        logout.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View arg0) {
+                // Logout current user
+                ParseUser.logOut();
+                finish();
+            }
+        });
     }
 
     @Override
