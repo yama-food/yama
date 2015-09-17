8178e72715f5204323174c5528464c69e60b34fd
valentinerutto <rutto28@gmail.com>
2015-09-17 23:35:30 +0300

    retrive data from parse and sort locations in ascending order

--- app/src/main/java/com/valentine/yama/Neighborhood.java
+++ app/src/main/java/com/valentine/yama/Neighborhood.java
@@ -6,10 +6,13 @@ import android.location.Location;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import com.parse.ParseException;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
@@ -26,7 +29,7 @@ public class Neighborhood extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.listview);
+        setContentView(R.layout.activity_neighborhood);
         // Execute RemoteDataTask AsyncTask
         new RemoteDataTask().execute();
     }
@@ -35,6 +38,15 @@ public class Neighborhood extends Activity {
 
         @Override
         protected Void doInBackground(Void... params) {
+            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
+                    "Neighborhoods");
+            query.orderByDescending("location");
+            try {
+                ob = query.find();
+            } catch (ParseException e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
             return null;
         }
 
