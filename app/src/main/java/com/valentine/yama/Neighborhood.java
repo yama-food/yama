0db6cc2a971fd5ae33ca54677806a9b2222d658f
valentinerutto <rutto28@gmail.com>
2015-09-17 23:04:30 +0300

    added listview on neighborhood layout

--- app/src/main/java/com/valentine/yama/Neighborhood.java
+++ app/src/main/java/com/valentine/yama/Neighborhood.java
@@ -1,17 +1,27 @@
 package com.valentine.yama;
 
+import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseQueryAdapter;
+import com.valentine.yama.gettersandsetters.Neighbor;
 
 public class Neighborhood extends ListView {
 
+    private ParseQueryAdapter<Neighbor> mainAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_neighborhood);
+
+        getListView().setClickable(true);
+        mainAdapter = new ParseQueryAdapter<Neighborhoods>(this, Neighborhood.class);
+        mainAdapter.setTextKey("location");
+        setListAdapter(mainAdapter);
     }
 
 }
