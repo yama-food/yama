9acca91b493abfefa224041b4cb405d43ad6b7ff
valentinerutto <rutto28@gmail.com>
2015-09-17 18:46:49 +0300

    button place on welcome to start neighborhood activity

---
+++ app/src/main/java/com/valentine/yama/NeighborhoodsMain.java
@@ -0,0 +1,7 @@
+package com.valentine.yama;
+
+/**
+ * Created by valentine on 9/17/15.
+ */
+public class NeighborhoodsMain {
+}
--- app/src/main/java/com/valentine/yama/Welcome.java
+++ app/src/main/java/com/valentine/yama/Welcome.java
@@ -1,17 +1,28 @@
 package com.valentine.yama;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Welcome extends ActionBarActivity {
-
+Button place;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        Button place = (Button) findViewById(R.id.btn);
+
+        place.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(new Intent(Welcome.this, NeighborhoodsMain.class));
+            }
+        });
     }
 
     @Override
