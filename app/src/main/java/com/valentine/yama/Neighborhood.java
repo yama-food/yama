a05a127c08c10dc08327486581a967b1c5792bb8
valentinerutto <rutto28@gmail.com>
2015-09-17 23:11:26 +0300

    get view from neighborhoodlayout and execute remotetaskasync method

--- app/src/main/java/com/valentine/yama/Neighborhood.java
+++ app/src/main/java/com/valentine/yama/Neighborhood.java
@@ -1,27 +1,33 @@
 package com.valentine.yama;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.location.Location;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import com.parse.ParseObject;
 import com.parse.ParseQuery;
 import com.parse.ParseQueryAdapter;
 import com.valentine.yama.gettersandsetters.Neighbor;
 
-public class Neighborhood extends ListView {
+import java.util.List;
 
-    private ParseQueryAdapter<Neighbor> mainAdapter;
+public class Neighborhood extends Activity {
+
+    ListView listview;
+    List<ParseObject> ob;
+    ProgressDialog mProgressDialog;
+    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        getListView().setClickable(true);
-        mainAdapter = new ParseQueryAdapter<Neighborhoods>(this, Neighborhood.class);
-        mainAdapter.setTextKey("location");
-        setListAdapter(mainAdapter);
+        setContentView(R.layout.listview_main);
+        // Execute RemoteDataTask AsyncTask
+        new RemoteDataTask().execute();
     }
 
 }
