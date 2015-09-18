56ff0899270d09079031bc249c7a29cd10b34a83
valentinerutto <rutto28@gmail.com>
2015-09-18 04:33:33 +0300

    fab dependencies on buildgradle

--- app/src/main/java/com/valentine/yama/Location.java
+++ app/src/main/java/com/valentine/yama/Location.java
@@ -1,14 +1,15 @@
-package com.valentine.yama.gettersandsetters;
+package com.valentine.yama;
 
 import com.parse.ParseClassName;
 import com.parse.ParseObject;
 
 /**
- * Created by valentine on 9/17/15.
+ * Created by valentine on 9/18/15.
  */
-@ParseClassName("Neighbourhoods")
-public class Neighbor extends ParseObject{
-    public Neighbor(){
+@ParseClassName("Task")
+
+public class Location extends ParseObject {
+    public Location(){
 
     }
 
--- app/src/main/java/com/valentine/yama/Neighborhood.java
+++ app/src/main/java/com/valentine/yama/Neighborhood.java
@@ -1,80 +1,17 @@
 package com.valentine.yama;
 
 import android.app.Activity;
-import android.app.ProgressDialog;
-import android.location.Location;
-import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 
-import com.parse.ParseException;
-import com.parse.ParseObject;
-import com.parse.ParseQuery;
-import com.parse.ParseQueryAdapter;
-import com.valentine.yama.gettersandsetters.Neighbor;
-
-import java.util.List;
 
 public class Neighborhood extends Activity {
 
-    ListView listview;
-    List<ParseObject> ob;
-    ProgressDialog mProgressDialog;
-    ArrayAdapter<String> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_neighborhood);
-        // Execute RemoteDataTask AsyncTask
-        new RemoteDataTask().execute();
+     
     }
 
-    private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            ParseQuery<ParseObject> query = new ParseQuery<ParseObject>(
-                    "Neighborhoods");
-            query.orderByDescending("location");
-            try {
-                ob = query.find();
-            } catch (ParseException e) {
-                Log.e("Error", e.getMessage());
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            mProgressDialog = new ProgressDialog(Neighborhood.this);
-            mProgressDialog.setTitle("loading locations");
-            mProgressDialog.setMessage("Loading...");
-            mProgressDialog.setIndeterminate(false);
-            mProgressDialog.show();
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            // Locate the listview in listview_main.xml
-            listview = (ListView) findViewById(R.id.listview);
-            // Pass the results into an ArrayAdapter
-            adapter = new ArrayAdapter<String>(Neighborhood.this,
-                    R.layout.activity_neighborhood);
-            // Retrieve object "location" from Parse.com database
-            for (ParseObject location : ob) {
-                adapter.add((String) location.get("name"));
-            }
-            // Binds the Adapter to the ListView
-            listview.setAdapter(adapter);
-            // Close the progressdialog
-            mProgressDialog.dismiss();
-        }
-    }
 }
